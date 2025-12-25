1e4b07f3e7d872ef48d57a46de86740e0f349c09
swayansh07 <swayanshprusty@gmail.com>
2025-12-25 15:50:11 +0530

    Create GCDorHCF.java

---
+++ GCDorHCF.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public static int gcd(int a, int b)
+    {
+     while (b != 0) 
+        {
+            int temp = b;     
+            b = a % b;        
+            a = temp;         
+        }
+        return a;  
+    }
+}
