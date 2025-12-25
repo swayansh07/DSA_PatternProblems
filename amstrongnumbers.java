f2fc17b6fabad634a923bd99e320da0637031a7b
swayansh07 <swayanshprusty@gmail.com>
2025-12-25 15:57:38 +0530

    Create amstrongnumbers.java

---
+++ amstrongnumbers.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    static boolean armstrongNumber(int n) 
+    {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0)
+        {
+            int digit = n % 10;      
+            sum += digit * digit * digit; 
+            n = n / 10;     
+        }
+
+        return sum == original; 
+    }
+}
