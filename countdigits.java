0a90982250398d74845161cecbcacb09313c2a61
swayansh07 <swayanshprusty@gmail.com>
2025-12-25 15:17:00 +0530

    Create countdigits.java

---
+++ countdigits.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int countDigits(int n) 
+    {
+         int count = 0;
+
+        if (n == 0) {
+            return 1;
+        }
+
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+
+        return count;
+        
+    }
+}
